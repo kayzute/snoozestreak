44f8b7dd2066f9e9ba0bac7b53d71c56c895c452
Samuel Williams <samuel.williams@uconn.edu>
2025-07-14 22:33:24 -0400

    streaks and goal check functionality

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Dashboard.css';
+// git test
 
 // format for time
 function formatDuration(ms) {
